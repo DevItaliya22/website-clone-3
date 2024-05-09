cef18f6f970a54fcc34a39f653946b4a904ac175
Dev <devitaliya22@example.com>
2024-05-09 22:20:28 +0530

    Second commit-4

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('.main'),
-//     smooth: true
-// });
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.main'),
+    smooth: true
+});
 
 var elem = document.querySelector("#elem-container")
 var fixedImage = document.querySelector(".fixed-image")
